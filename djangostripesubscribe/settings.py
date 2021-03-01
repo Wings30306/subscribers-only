74e57ef8b1133ae60f24e894a44efb16a3231a5e
Joke Heyndels <jo_hannah@outlook.com>
2021-03-01 16:08:28 +0000

    Update secret key setting (unsaved file was pushed with old secret key)

--- djangostripesubscribe/settings.py
+++ djangostripesubscribe/settings.py
@@ -10,6 +10,8 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
+import os
+import env
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'cii0x__z6gxsienw^7wn$6(^*@=y$6j!jafzj+=uoe_mghcq_5'
+SECRET_KEY = os.environ.get("SECRET_KEY", "")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -119,3 +121,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STRIPE_PUBLISHABLE_KEY = os.environ.get("STRIPE_PK")
+STRIPE_SECRET_KEY = os.environ.get("STRIPE_SK")
